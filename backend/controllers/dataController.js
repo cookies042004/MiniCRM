e473066fb64fa5c9c294d04a28a6101c1ad69ec2
Akhil Puri <ap4723@srmist.edu.in>
2025-05-12 02:36:37 +0530

    updated backend

--- backend/controllers/dataController.js
+++ backend/controllers/dataController.js
@@ -1,4 +1,4 @@
-const Customer = require('../models/Customer');
+const Customer = require('../models/customer');
 const Order = require('../models/Order');
 
 exports.ingestCustomer = async (req, res) => {
